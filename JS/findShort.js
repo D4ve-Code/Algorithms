1ed70f6b83a3fb0eeab7b33a074d8f65c3bcbfae
D4ve-Code <78406260+D4ve-Code@users.noreply.github.com>
2023-01-23 10:02:01 +0100

    NewAlgoFindShort

--- JS/findShort.js
+++ JS/findShort.js
@@ -6,11 +6,24 @@
 // findShort("Prachett is the best author in the world ") // 2
 
 
+/* Je balaye l'ensemble de la sentence, je trouve la range? la plus petite et je la retourne dans un résultat
+
+1. je transforme la phrase en tableau déléments
+2. j'ai fait un for pour balayer les élements (ou .map)
+3. filter ?
+4. je renvoie l'élement en string 
+*/
+
 console.log("start");
 console.log("-------------------------------------------------");
 
-function findShort(sentence){
-
+const findShort = (sentence) => {
+    const tableau = sentence.split(' ')
+    const result = []
+    for (let i = 0; i < tableau.length; i++){
+        result.push(tableau[i].length)
+    }
+    return (Math.min(...result))
 }
 console.log(findShort("Prachett is the best author in the world")); 
 
